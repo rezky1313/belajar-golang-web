df1c0b1f9ac3166509ced3bf4b26c6b1c399b9b0
rezky1313 <rezkywahyudi1313@gmail.com>
2024-11-22 21:03:44 +0700

    Pengenalan Client Server

---
+++ 2. Client dan Server_test.go
@@ -0,0 +1,32 @@
+package belajar_golang_web
+
+/*
+Client dan server
+-Web adalah aplikasi berbasis client dan server
+-client server merupakan konsep arsitektur aplikasi yang menghubungkan dua pihak sistem client dan sistem server
+-saling berkomunikasi melalui internet atau komputer yang sama
+
+[client]---------[server]
+-client melakukan request ke server
+-server mengembalikan data yang direquest dan diterima kembali oleh client
+
+Tugas client dan server
+-aplikasi client bertugas mengirim request ke server, dan menerima response dari server
+-sedangkan aplikasi server menerima request dari sever, memproses data dan mengembalikan hasil proses ke client
+
+Keuntungan Client dan Server
+-perubahan aplikasi bisa dilakukan dengan mudah di server, tanpa harus membuat perubahan di client
+apalagi jika client berada dilokasi yang sulit dijangkau, contohnya jika ada salah logic pada server kita cukup ubah
+dari sisi servernya saja, karena client tidak diberi logic sama sekali, client cuma menerima responsenya
+-bisa digunakan oleh banyak client secara bersamaan, walaupun server tidak banyak
+-bisa diakses dimana saja, asal terhubung dengan internet atau satu jaringan dengan server
+
+Contoh Client server
+-Web adalah salah satu contoh client server
+-Aplikasi yang berperan sebagai client adalah web browser
+-yang bertugas sebagai server adalah Web Server, dimana didalam webs server terdapat kode program kita
+
+
+
+
+*/
